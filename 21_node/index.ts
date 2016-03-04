2c0b72c2d3e45ced6929d81e1d17ae6e98daa5bb
Thomas Anciaux <zebigbozz@gmail.com>
2016-03-04 15:16:49 +0100

    Added CORS headers for later excercises

--- 21_node/index.ts
+++ 21_node/index.ts
@@ -5,6 +5,12 @@ import * as root from './routes/root-route';
 
 var app = express();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Headers", "OPTIONS, GET, POST, PUT");
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 app.get('/', root.getRoot);
 
