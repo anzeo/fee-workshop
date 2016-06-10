905ddf1b17b9bd5f87f2d5a40dbe25003570998c
glenndejaeger <glenn.dejaeger@gmail.com>
2016-06-10 17:17:24 +0200

    Path to src file added to spec

--- 21_testing/test/my-first-spec.ts
+++ 21_testing/test/my-first-spec.ts
@@ -1,15 +1,17 @@
+/// <reference path="../src/my-first-script.ts"/>
+
 describe("An add function", () => {
     it("can add 2 integers", ()=> {
-        expect(add(1,1)).toEqual(2);
-        expect(add(2,3)).toEqual(5);
+        expect(add(1, 1)).toEqual(2);
+        expect(add(2, 3)).toEqual(5);
     });
 
     it("can not add string values", ()=> {
-        expect(add(1,"1")).not.toEqual(2);
-        expect(add("2",3)).not.toEqual(5);
+        expect(add(1, "1")).not.toEqual(2);
+        expect(add("2", 3)).not.toEqual(5);
 
-        expect(add(1,"1")).toEqual("11");
-        expect(add("2",3)).toEqual("23");
+        expect(add(1, "1")).toEqual("11");
+        expect(add("2", 3)).toEqual("23");
     });
 });
 
